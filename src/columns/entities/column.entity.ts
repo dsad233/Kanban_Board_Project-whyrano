c51e58c6a7e58211b1a2272cadd1dd7517e054d4
세계 <nw0417@gmail.com>
2024-03-18 20:51:31 +0900

    컬럼엔티티03.1820:51

--- src/columns/entities/column.entity.ts
+++ src/columns/entities/column.entity.ts
@@ -1,1 +1,26 @@
-export class Column { }
+
+import { IsString } from "class-validator";
+import { Cards } from "src/cards/entities/card.entity";
+import { BaseModel } from "src/common/entities/base.entity";
+import { Color } from "src/common/types/color.type";
+import { Column, Entity, OneToMany } from "typeorm";
+
+@Entity('columns')
+export abstract class Coulm extends BaseModel {
+
+    @IsString()
+    @Column({ unsigned: true })
+    boardId: number;
+
+    @Column()
+    title: string;
+
+    @Column({ type: 'enum', enum: Color, default: Color.BLACK })
+    color: Color;
+
+    @Column({ type: 'number', nullable: false })
+    orderByColumns: number;
+
+    @OneToMany(() => Cards, (card) => card.column)
+    cards: Cards[];
+}
