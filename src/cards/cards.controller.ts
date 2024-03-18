661766cfe3c517318918ef07c8ac4dcdf8bba59e
8860k <ysmkk@naver.com>
2024-03-18 21:26:30 +0900

    card entity/ control

--- src/cards/cards.controller.ts
+++ src/cards/cards.controller.ts
@@ -6,17 +6,21 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
 } from "@nestjs/common"
 import { CardsService } from "./cards.service";
 import { CreateCardDto } from "./dto/create-card.dto";
 import { UpdateCardDto } from "./dto/update-card.dto";
+import { AuthGuard } from "@nestjs/passport";
 
+
+@UseGuards(AuthGuard('jwt'))
 @Controller("cards")
 export class CardsController {
   constructor(private readonly cardsService: CardsService) {}
 
   @Post()
-  create(@Body() createCardDto: CreateCardDto) {
+  createCard(@Body() createCardDto: CreateCardDto) {
     return this.cardsService.create(createCardDto);
   }
 
@@ -25,16 +29,24 @@ export class CardsController {
     return this.cardsService.findAll();
   }
 
+  @Get('')
+  async getAllMessages(@Param('teamId') teamId: number) {
+    return await this.supportMessageService.getMessagesByTeamId(teamId);
+  }
+
   @Get(":id")
   findOne(@Param("id") id: string) {
     return this.cardsService.findOne(+id);
   }
 
   @Patch(":id")
-  update(@Param("id") id: string, @Body() updateCardDto: UpdateCardDto) {
+  updateCard(@Param("id") id: string, @Body() updateCardDto: UpdateCardDto) {
     return this.cardsService.update(+id, updateCardDto);
   }
 
+  // 카드 순서변경 & 이동
+
+  
   @Delete(":id")
   remove(@Param("id") id: string) {
     return this.cardsService.remove(+id);
