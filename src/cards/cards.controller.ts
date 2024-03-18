3b15ce6af1599e7008acbbef99ff715fa57e13c4
8860k <ysmkk@naver.com>
2024-03-18 21:59:46 +0900

    cards.controller.ts 0.7

--- src/cards/cards.controller.ts
+++ src/cards/cards.controller.ts
@@ -12,43 +12,65 @@ import { CardsService } from "./cards.service";
 import { CreateCardDto } from "./dto/create-card.dto";
 import { UpdateCardDto } from "./dto/update-card.dto";
 import { AuthGuard } from "@nestjs/passport";
-
+import { Users } from "src/users/entities/user.entity";       
 
 @UseGuards(AuthGuard('jwt'))
 @Controller("cards")
 export class CardsController {
   constructor(private readonly cardsService: CardsService) {}
 
-  @Post()
-  createCard(@Body() createCardDto: CreateCardDto) {
-    return this.cardsService.create(createCardDto);
-  }
-
-  @Get()
-  findAll() {
-    return this.cardsService.findAll();
-  }
-
+  
+  // 카드 목록 조회
   @Get('')
-  async getAllMessages(@Param('teamId') teamId: number) {
-    return await this.supportMessageService.getMessagesByTeamId(teamId);
+  async findAllCard () {
+    return await this.cardsService.findAll();
   }
-
+  
+  // 카드 상세 조회
   @Get(":id")
   findOne(@Param("id") id: string) {
     return this.cardsService.findOne(+id);
   }
 
+  // 카드 생성
+  @Post()
+  async createCard(
+    @UserInfo() user: Users,
+    @Param('cardId') cardId: number,
+    @Body() CreateCardDto: CreateCardDto,
+  ) {
+    await this.cardsService.createCard(
+      userInfo.name,
+      CreateCardDto.content,
+    );
+  }
+
+  // 카드 수정 / 담당자 변경
   @Patch(":id")
-  updateCard(@Param("id") id: string, @Body() updateCardDto: UpdateCardDto) {
-    return this.cardsService.update(+id, updateCardDto);
+  async updateCard(
+    @UserInfo() user: Users,
+    @Param('id') id: number,
+    @Body() UpdateCardDto: UpdateCardDto,
+  ) {
+    await this.cardsService.updateCard(
+      userInfo.name,
+      UpdateCardDto.content,
+    );
   }
 
   // 카드 순서변경 & 이동
+  /* 코드 */
 
-  
+  // 카드 삭제
   @Delete(":id")
-  remove(@Param("id") id: string) {
-    return this.cardsService.remove(+id);
+  async deleteCard(
+    @UserInfo() user: Users, 
+    @Param('id') id: number) {
+    await this.cardsService.deleteCard(id, user.id);
   }
 }
+
+function UserInfo(): (target: CardsController, propertyKey: "createCard", parameterIndex: 0) => void {
+  throw new Error("Function not implemented.");
+}
+
